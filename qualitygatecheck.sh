f1a4f6a528c616d3cc8111c11da2ebb2925b4c16
Anjana-Devops2023 <154388003+Anjana-Devops2023@users.noreply.github.com>
2023-12-26 20:04:04 -0500

    Update qualitygatecheck.sh

--- qualitygatecheck.sh
+++ qualitygatecheck.sh
@@ -3,8 +3,8 @@ curl -u admin:Anjana http://18.218.195.244:9000/api/qualitygates/project_status?
 scan_status=$(cat check.json | grep -i status | cut -d ":" -f 3 | cut -d "," -f 1 | tr -d '"')
 if [[ $scan_status -eq OK ]]
 then
-  echo "sonar scan status is $scan_status........!!!"
+  echo "Anjana sonar scan status is $scan_status........!!!"
 else
-  echo "Sonar scan status is $scan_status please check ......!"
+  echo "Anjana Sonar scan status is $scan_status please check ......!"
   exit 1;
 fi
